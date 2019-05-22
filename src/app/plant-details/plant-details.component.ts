c3fb1df2989981d7a5c765ee6d508f87b665ae83
samah-gaber <samah.gaber.94@gmail.com>
2019-05-22 09:27:53 +0200

    edit 1

---
+++ src/app/plant-details/plant-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-plant-details',
+  templateUrl: './plant-details.component.html',
+  styleUrls: ['./plant-details.component.scss']
+})
+export class PlantDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
