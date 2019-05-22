3700e5e6c9ac8df22ab4f8c3faa63e1a3092abe5
samah-gaber <samah.gaber.94@gmail.com>
2019-05-22 10:21:51 +0200

    router outlet added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PlantDetailsComponent } from './plant-details/plant-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: PlantDetailsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
