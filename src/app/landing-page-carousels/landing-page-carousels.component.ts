64720ce45385fa0996b2ebb3912cf188a32c5fae
Radwa Mabrouk <radwaamabrouk@gmail.com>
2019-05-26 20:51:00 +0200

    Created special slider for plant cat, top slider, compressed images

--- src/app/landing-page-carousels/landing-page-carousels.component.ts
+++ src/app/landing-page-carousels/landing-page-carousels.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 
 })
 export class LandingPageCarouselsComponent implements OnInit {
-  images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
+  // images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
   constructor() {
 
    }
