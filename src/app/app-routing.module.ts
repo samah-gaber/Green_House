b9fa8a95818fab08d7599a75fd3b3270f4937080
Radwa Mabrouk <radwaamabrouk@gmail.com>
2019-05-30 01:02:52 +0200

    Fixed routing form home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LandingPageModuleModule} from './landing-page/landing-page-module.module';
 import { PlantDetailsPageModule } from './plant-details-page-module/plant-details-page.module';
 
 const routes: Routes = [
@@ -18,6 +19,7 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [
     RouterModule,
+    LandingPageModuleModule,
     PlantDetailsPageModule
   ]
 })
